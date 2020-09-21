96684c09366d18ca73e17003443cb7ac59133fef
dman54 <dr.d-man54@yandex.ru>
2020-09-21 17:00:04 +0700

    mobile menu

--- js/script.js
+++ js/script.js
@@ -1,13 +1,16 @@
 $(".fa-bars").on("click", function (e) {
-  $(".navbar-nav_added").toggleClass("active");
-  $("body").toggleClass("modal-open");
-  $("#closeicon_added").toggleClass("active");
+  $(".topmenu").toggleClass("active");
+  $("body").toggleClass("menu-open");
+});
+$("#sidebar-overlay1").on("click", function () {
+  $(".topmenu").removeClass('active');
+  $("body").removeClass('menu-open');
 });
 $(".hostel-buttons .get-call").on("click", function (e) {
   $(".modal-popup").addClass('active');
-  $('body').addClass('modal-open');
+  $("body").addClass('modal-open');
 });
-$("#closeicon2_added, #sidebar-overlay2").on('click', function () {
+$("#closeicon2_added, #sidebar-overlay2").on("click", function () {
   $(".modal-popup").removeClass('active');
-  $('body').removeClass('modal-open');
+  $("body").removeClass('modal-open');
 });
