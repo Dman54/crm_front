987977f0c38b75707b12bf5f4c5f91793af378e7
dman54 <dr.d-man54@yandex.ru>
2020-11-04 23:06:14 +0700

    все удобства

--- js/script.js
+++ js/script.js
@@ -3,28 +3,34 @@ $(".fa-bars").on("click", function (e) {
   $("body").toggleClass("menu-open");
 });
 $("#sidebar-overlay1").on("click", function () {
-  $(".topmenu").removeClass('active');
-  $("body").removeClass('menu-open');
+  $(".topmenu").removeClass("active");
+  $("body").removeClass("menu-open");
 });
 $(".hostel-buttons .get-call").on("click", function (e) {
-  $(".modal-popup").addClass('active');
-  $("body").addClass('modal-open');
+  $(".modal-popup").addClass("active");
+  $("body").addClass("modal-open");
 });
 $("#closeicon2_added, #sidebar-overlay2").on("click", function () {
-  $(".modal-popup").removeClass('active');
-  $("body").removeClass('modal-open');
+  $(".modal-popup").removeClass("active");
+  $("body").removeClass("modal-open");
 });
 $("#add-reservation-form").submit(function (e) {
   $(this).find("[jq-required]").removeClass("empty-error");
   $(this).find(".empty-error-text").remove();
   let isError = false;
-  $(this).find("[jq-required]").each(function (index) {
-    if (!$(this).val()) {
-      $(this).addClass("empty-error");
-      $(this).after("<div class='empty-error-text'>" + $(this).attr("error-text") + "</div>");
-      isError = true;
-    }
-  });
+  $(this)
+    .find("[jq-required]")
+    .each(function (index) {
+      if (!$(this).val()) {
+        $(this).addClass("empty-error");
+        $(this).after(
+          "<div class='empty-error-text'>" +
+            $(this).attr("error-text") +
+            "</div>"
+        );
+        isError = true;
+      }
+    });
   if (isError) {
     e.preventDefault();
   }
@@ -37,87 +43,96 @@ $("#add-reservation-form").submit(function (e) {
 // crm_front_object.html
 // $(function () {
 $(window).on("load", function () {
-  $(".object-room").each(function() {
+  $(".object-room").each(function () {
     let rightHeight = $(this).find(".object-room-facilities").height();
-    if ($(window).width() > 800) {
-      let leftHeight = $(this).find(".object-room-left").height();
-      if (rightHeight > leftHeight) {
-        $(this).find(".object-room-facilities").height(leftHeight - 24);
-        $(this).find(".show_more_facilities").addClass("active");
-        $(this).find(".object-room-facilities").addClass("active");
-      }
+    if (rightHeight > 102) {
+      $(this).find(".show_more_facilities").addClass("active");
     } else {
-      if (rightHeight > 140) {
-        $(this).find(".show_more_facilities").addClass("active");
-        $(this).find(".object-room-facilities").addClass("active");
-        $(this).find(".object-room-facilities-wrapper").height(162);
-      } else {
-        $(this).find(".object-room-facilities-wrapper").height($(this).find(".object-room-facilities").height());
-      }
+      $(this).find(".object-room-facilities-wrapper").height("auto");
     }
   });
-  $(".show_more_facilities").on("click", function() {
+  $(".show_more_facilities").on("click", function () {
+    let topHeight = "64px";
+    if ($(window).width() <= 745) {
+      topHeight = "99px";
+    }
     if ($(this).hasClass("showing")) {
       $(this).parent().removeClass("active");
-      $(this).prev().height($(this).parent().height() - 24);
+      $(this).prev().height($(this).parent().height());
       $(this).removeClass("showing");
-      $(this).css('top','unset');
+      $(this).css("top", topHeight);
     } else {
       $(this).parent().addClass("active");
-      $(this).prev().height('auto');
+      $(this).prev().height("auto");
       $(this).addClass("showing");
-      $(this).css('top',($(this).prev().height()));
+      $(this).css("top", $(this).prev().height());
     }
   });
 
   // slider
-  let liCount = $('.slides li').length;
+  let liCount = $(".slides li").length;
   let curIndex = 1;
-  $('.slider-all-number').text(liCount);
-  $('.slider-cur-number').text(curIndex);
+  $(".slider-all-number").text(liCount);
+  $(".slider-cur-number").text(curIndex);
   if (liCount == 1) {
-    $('.slider-prev').addClass('disabled');
-    $('.slider-next').addClass('disabled');
+    $(".slider-prev").addClass("disabled");
+    $(".slider-next").addClass("disabled");
   }
   if (curIndex == 1) {
-    $('.slider-prev').addClass('disabled');
+    $(".slider-prev").addClass("disabled");
   }
   if (curIndex == liCount) {
-    $('.slider-next').addClass('disabled');
+    $(".slider-next").addClass("disabled");
   }
-  $('.slider-wrapper').height($('.slides li img').eq(curIndex - 1).height());
+  $(".slider-wrapper").height(
+    $(".slides li img")
+      .eq(curIndex - 1)
+      .height()
+  );
 
   function move() {
-    $('.slider-next').removeClass('disabled');
-    $('.slider-prev').removeClass('disabled');
-    $('.slider-wrapper ul').css({ "-webkit-transform": "translateX(" + -(curIndex - 1) * 100 + "%" });
-    $('.slider-wrapper ul').css({ "transform": "translateX(" + -(curIndex - 1) * 100 + "%" });
-    $('.slider-dot').removeClass('active');
-    $('.slider-dot').eq(curIndex - 1).addClass('active');
-    $('.slider-cur-number').text(curIndex);
-    $('.slides li').removeClass('active');
-    $('.slides li').eq(curIndex - 1).addClass('active');
-    $('.slider-wrapper').height($('.slides li img').eq(curIndex - 1).height());
+    $(".slider-next").removeClass("disabled");
+    $(".slider-prev").removeClass("disabled");
+    $(".slider-wrapper ul").css({
+      "-webkit-transform": "translateX(" + -(curIndex - 1) * 100 + "%",
+    });
+    $(".slider-wrapper ul").css({
+      transform: "translateX(" + -(curIndex - 1) * 100 + "%",
+    });
+    $(".slider-dot").removeClass("active");
+    $(".slider-dot")
+      .eq(curIndex - 1)
+      .addClass("active");
+    $(".slider-cur-number").text(curIndex);
+    $(".slides li").removeClass("active");
+    $(".slides li")
+      .eq(curIndex - 1)
+      .addClass("active");
+    $(".slider-wrapper").height(
+      $(".slides li img")
+        .eq(curIndex - 1)
+        .height()
+    );
   }
   function moveLeft() {
     curIndex++;
     move();
     if (curIndex == liCount) {
-      $('.slider-next').addClass('disabled');
+      $(".slider-next").addClass("disabled");
     }
   }
   function moveRight() {
     curIndex--;
     move();
     if (curIndex == 1) {
-      $('.slider-prev').addClass('disabled');
+      $(".slider-prev").addClass("disabled");
     }
   }
 
-  $('.slider-prev').click(function () {
+  $(".slider-prev").click(function () {
     moveRight();
   });
-  $('.slider-next').click(function () {
+  $(".slider-next").click(function () {
     moveLeft();
   });
 });
@@ -125,13 +140,19 @@ $("#calc-form").submit(function (e) {
   $(this).find("[jq-required]").removeClass("empty-error");
   $(this).find(".empty-error-text").remove();
   let isError = false;
-  $(this).find("[jq-required]").each(function (index) {
-    if (!$(this).val()) {
-      $(this).addClass("empty-error");
-      $(this).after("<div class='empty-error-text'>" + $(this).attr("error-text") + "</div>");
-      isError = true;
-    }
-  });
+  $(this)
+    .find("[jq-required]")
+    .each(function (index) {
+      if (!$(this).val()) {
+        $(this).addClass("empty-error");
+        $(this).after(
+          "<div class='empty-error-text'>" +
+            $(this).attr("error-text") +
+            "</div>"
+        );
+        isError = true;
+      }
+    });
   if (isError) {
     e.preventDefault();
   }
@@ -150,12 +171,17 @@ $("#calc-form").submit(function (e) {
 });
 $(".table-pricetypes-show-more").on("click", function () {
   $(this).toggleClass("active");
-  $(this).closest(".table-pricetypes-row").find(".table-pricetypes-all").toggleClass("active");
+  $(this)
+    .closest(".table-pricetypes-row")
+    .find(".table-pricetypes-all")
+    .toggleClass("active");
 });
 $(".block-toggle").on("click", function () {
   $(".block-toggle").removeClass("active");
   $(".block-toggled").removeClass("active");
   $(this).addClass("active");
-  $(".block-toggled").eq($(".block-toggle").index($(this))).addClass('active');
+  $(".block-toggled")
+    .eq($(".block-toggle").index($(this)))
+    .addClass("active");
 });
 // // crm_front_object.html
